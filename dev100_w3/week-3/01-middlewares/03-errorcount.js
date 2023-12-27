125bf2d61344fc10834793f1e743eebb5654421f
Karan Jodha <karan@MacBook-Pro.local>
2023-12-27 02:37:57 -0500

    Middleware soln

--- dev100_w3/week-3/01-middlewares/03-errorcount.js
+++ dev100_w3/week-3/01-middlewares/03-errorcount.js
@@ -8,7 +8,8 @@ let errorCount = 0;
 // You have been given an express server which has a few endpoints.
 // Your task is to
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
-// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
+// 2. Maintain the errorCount variable whose value should go up every time there is an 
+// exception in any endpoint
 
 app.get('/user', function(req, res) {
   throw new Error("User not found");
@@ -22,5 +23,10 @@ app.post('/user', function(req, res) {
 app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
+
+app.use(function(err, req, res, next) {
+    errorCount+=1;
+    res.status(404).send()
+})
 
 module.exports = app;
